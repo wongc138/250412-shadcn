5bceb7b8cbd1c1d42c7979622ee5387fda696ece
wongc138 <wongc138@uni.coventry.ac.uk>
2025-04-12 21:42:00 +0100

    login page fix

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -4,26 +4,26 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import Link from "next/link";
 
 export default function LoginPage() {
-    return
-    <>
-        <div> Login </div>
-        <Card>
-            <CardHeader>
-                <CardTitle> Login </CardTitle>
-                <CardDescription>
-                    Login to your SupportMe account
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                login form
-            </CardContent>
-            <CardFooter>
-                <small> Don't have an account? </small>
-                <Button asChild variant="outline" size={"sm"} >
-                    <Link href="/sign-up" >Sign Up</Link>
-                </Button>
-            </CardFooter>
-        </Card>
-    </>
-
+    return (
+        // <div> Login </div>
+        <>
+            <Card>
+                <CardHeader>
+                    <CardTitle> Login </CardTitle>
+                    <CardDescription>
+                        Login to your SupportMe account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    login form
+                </CardContent>
+                <CardFooter>
+                    <small> Don't have an account? </small>
+                    <Button asChild variant="outline" size={"sm"} >
+                        <Link href="/sign-up" >Sign Up</Link>
+                    </Button>
+                </CardFooter>
+            </Card>
+        </>
+    );
 }
