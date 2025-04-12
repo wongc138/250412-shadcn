3c6eed1173d655378065f279f94dbce7f5ac7163
wongc138 <wongc138@uni.coventry.ac.uk>
2025-04-12 21:19:05 +0100

    Login page card

---
+++ app/(logged-out)/login/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
+export default function LoginPage() {
+    return
+    <>
+        <div> Login </div>
+        <Card>
+            <CardHeader>
+                <CardTitle> Login </CardTitle>
+                <CardDescription>
+                    Login to your SupportMe account
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                login form
+            </CardContent>
+            <CardFooter>
+                <small> Don't have an account? </small>
+                <Button asChild variant="outline" size={"sm"} >
+                    <Link href="/sign-up" >Sign Up</Link>
+                </Button>
+            </CardFooter>
+        </Card>
+    </>
+
+}
